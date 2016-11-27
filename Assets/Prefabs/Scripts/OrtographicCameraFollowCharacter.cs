e1dd62f4949a600e8d8a8f8de62772924780e9a4
rulo7 <rulo2305@gmail.com>
2016-11-27 12:39:02 +0100

    - Finished followe Y camera and fixed some problems with the player control

--- Assets/Prefabs/Scripts/OrtographicCameraFollowCharacter.cs
+++ Assets/Prefabs/Scripts/OrtographicCameraFollowCharacter.cs
@@ -5,11 +5,9 @@ using System.Collections.Generic;
 public class OrtographicCameraFollowCharacter : MonoBehaviour {
 	// target GO to follow
 	public GameObject target;
-	// left limit for the scene camera
-	public GameObject leftLimit;
-	// right limit for the scene camera
-	public GameObject rightLimit;
-	// main camera of the scene
+	// the limits of the scenario
+	public Renderer limits;
+	// scene camera
 	public Camera camera;
 	// speed camera follows the target
 	public float speed;
@@ -33,11 +31,12 @@ public class OrtographicCameraFollowCharacter : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		int directionTargetIndex = updateTargetIndex();
-		float position = getCameraPositionX(directionTargetIndex);
+		float positionX = getCameraPositionX(directionTargetIndex);
+		float positionY = getCameraPositionY();
 		float step = speed * Time.deltaTime;
-		Vector3 newPosition = new Vector3 (target.transform.position.x + (directionTargetIndex * offset), camera.transform.position.y, camera.transform.position.z);
-		if (position != -1){
-			newPosition = new Vector3 (position, camera.transform.position.y, camera.transform.position.z);	
+		Vector3 newPosition = new Vector3 (target.transform.position.x + (directionTargetIndex * offset), positionY, camera.transform.position.z);
+		if (positionX != -1){
+			newPosition = new Vector3 (positionX, positionY, camera.transform.position.z);	
 		}
 
 		camera.transform.position = Vector3.MoveTowards(camera.transform.position, newPosition, step);
@@ -59,8 +58,12 @@ public class OrtographicCameraFollowCharacter : MonoBehaviour {
 	private float getCameraPositionX(int directionTargetIndex){
 		BoxCollider2D box = target.GetComponent<BoxCollider2D>();
 		float targetColliderHalfWidth = box != null ? box.size.x / 2.0f : 0.0f;
-		float rightPointLimit = rightLimit.transform.position.x - (cameraWidth / 2.0f);
-		float leftPointLimit = leftLimit.transform.position.x + (cameraWidth / 2.0f);
+
+		float rightLimit = limits.bounds.center.x + (limits.bounds.size.x / 2.0f);
+		float leftLimit = limits.bounds.center.x - (limits.bounds.size.x / 2.0f);
+
+		float rightPointLimit = rightLimit - (cameraWidth / 2.0f);
+		float leftPointLimit = leftLimit + (cameraWidth / 2.0f);
 		float targetPosition = target.transform.position.x;
 		if (targetPosition - targetColliderHalfWidth < (leftPointLimit - (offset *directionTargetIndex))) {
 			return leftPointLimit;
@@ -69,5 +72,27 @@ public class OrtographicCameraFollowCharacter : MonoBehaviour {
 			return rightPointLimit;
 		}
 		return -1;
+	}
+
+
+	private float getCameraPositionY(){
+		BoxCollider2D box = target.GetComponent<BoxCollider2D>();
+		float targetColliderHalfHeight = box != null ? box.size.y / 2.0f : 0.0f;
+
+		float topLimit = limits.bounds.center.y + (limits.bounds.size.y / 2.0f);
+		float bottomLimit = limits.bounds.center.y - (limits.bounds.size.y / 2.0f);
+
+		float topPointLimit = topLimit - (cameraHeight / 2.0f);
+		float bottomPointLimit = bottomLimit + (cameraHeight / 2.0f);
+
+		float targetPosition = target.transform.position.y;
+
+		if (targetPosition - targetColliderHalfHeight <= (bottomPointLimit - 0.01)) {
+			return bottomPointLimit;
+		}
+		if (targetPosition + targetColliderHalfHeight >= (topPointLimit +  0.01)) {
+			return topPointLimit;
+		}
+		return target.transform.position.y;
 	}
 }
